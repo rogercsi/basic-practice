57ff40fc27270afa990995ecf6d08dfca101c9bb
zouchengfu <zouchengfu@yuanbaobaoxian.com>
2024-04-23 21:32:07 +0800

    add

--- SemaphoreLimiter.java
+++ SemaphoreLimiter.java
@@ -3,25 +3,26 @@ import java.util.concurrent.TimeUnit;
 
 public class SemaphoreLimiter {
     public static void main(String[] args) {
-        Semaphore semaphore = new Semaphore(10);
+        Semaphore semaphore = new Semaphore(1);
         for(int i = 0; i < 100; i++) {
+            int finalI = i;
             Thread t = new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    doSomething(semaphore);
+                    doSomething(semaphore, finalI);
                 }
             });
             t.start();
         }
     }
-    private static  void doSomething(Semaphore semaphore) {
+    private static  void doSomething(Semaphore semaphore, int index) {
         boolean acquired = false;
         try {
-            acquired = semaphore.tryAcquire(3000, TimeUnit.MILLISECONDS);
+            acquired = semaphore.tryAcquire(1, TimeUnit.NANOSECONDS);
             if(acquired) {
-                System.out.println("执行业务逻辑");
+                System.out.println("执行业务逻辑 index:" + index);
             } else {
-                System.out.println("信号量未获取执行的逻辑");
+                System.out.println("信号量未获取执行的逻辑 index:" + index);
             }
         } catch (Throwable e) {
             e.printStackTrace();
