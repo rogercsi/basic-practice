41b59d8b76c8a98d299809f7c3fcc9f7d88390ce
zcf <rogercsizou@gmail.com>
2024-04-23 12:56:44 +0800

    first commit

---
+++ MultiThread.java
@@ -0,0 +1,54 @@
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class MultiThread {
+    class Task implements Runnable {
+        private LinkedBlockingQueue taskQueue = new LinkedBlockingQueue();
+        private AtomicBoolean running = new AtomicBoolean(true);
+
+        public void submitTask(Object task) throws InterruptedException {
+            taskQueue.put(task);
+        }
+
+        @Override
+        public void run() {
+            while (running.get()) {
+                try {
+                    Object task = taskQueue.take(); // 如果没有任务，会使线程阻塞，一旦有任务，会被唤醒
+                    doSomething(task);
+                } catch (Throwable e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        public void shutdown() {
+            if (running.compareAndSet(true, false)) {
+                System.out.println(Thread.currentThread() + " is stoped");
+            }
+        }
+
+        private void doSomething(Object task) throws InterruptedException {
+            System.err.println(task);
+            Thread.sleep(500);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        // Create an instance of MultiThread
+        MultiThread multiThread = new MultiThread();
+        // Now create an instance of Task within the MultiThread context
+        Task task = multiThread.new Task();
+        Thread thread = new Thread(task);
+        thread.setDaemon(true);
+        task.submitTask("task1");
+        thread.start();
+        for (int i = 0; i < 5; i++) {
+            task.submitTask(i);
+        }
+        // Wait for all tasks to finish before exiting
+        while (task.taskQueue.size() > 0) {
+            Thread.sleep(100); // adjust sleep time as needed
+        }
+    }
+}
